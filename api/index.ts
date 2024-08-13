68401c80b4f49e62eed88b1594f7edf7e034edf5
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-08-13 18:08:20 -0300

    b

--- api/index.ts
+++ api/index.ts
@@ -2,11 +2,20 @@ import { PrismaClient } from '@prisma/client';
 import { ZenStackMiddleware } from '@zenstackhq/server/express';
 import RestApiHandler from '@zenstackhq/server/api/rest';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
 const prisma = new PrismaClient();
+
+// Configuração do middleware CORS
+app.use(cors({
+    origin: '*', // Permite todas as origens. Ajuste conforme necessário.
+    methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+    credentials: true
+}));
 
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
